7be62a9ebb942db858b612de39fcb31343d85d07
niku <nikman33@mail.ru>
2017-02-12 16:03:49 +0300

    v2

--- app/src/main/java/com/andrew/niku/beatbox/BeatBoxActivity.java
+++ app/src/main/java/com/andrew/niku/beatbox/BeatBoxActivity.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 
 public class BeatBoxActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_beat_box);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.fragment_beat_box);
 
-    }
+  }
+
 }
